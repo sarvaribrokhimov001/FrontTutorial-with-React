eec35fa638ecef97a71869f6678c66656113a12a
ibrokhimovnba <ibrokhimovnba@gmail.com>
2026-03-21 00:08:27 +0500

    FrontTutorial

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import '../styles/Home.css';
 import Footer from '../components/Footer';
 import { Link } from "react-router-dom";
--- src/pages/Shorts.jsx
+++ src/pages/Shorts.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import '../styles/Shorts.css';
 
 const Shorts = () => {
